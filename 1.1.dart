1dec2581704ed96a91c2349f3b750e1675ccc19e
vladung66-cmyk <vladung66@gmail.com>
2026-03-12 13:30:43 +0500

    Add files via upload

---
+++ 1.1.dart
@@ -0,0 +1,10 @@
+void main() {
+  String name = 'Влад';
+  int age = 21;
+  double averageScore = 4.5;
+  bool isStudent = true;
+  print('Имя: $name ${name.runtimeType}');
+  print('Возраст: $age ${age.runtimeType}');
+  print('Средний балл: $averageScore ${averageScore.runtimeType}');
+  print('Студент: $isStudent ${isStudent.runtimeType}');
+}
