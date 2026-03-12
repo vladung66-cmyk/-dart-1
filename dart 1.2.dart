ffa6c41356c4f0da81f5eebaf22eb47e06844c41
vladung66-cmyk <vladung66@gmail.com>
2026-03-12 13:35:31 +0500

    Add files via upload

---
+++ dart 1.2.dart
@@ -0,0 +1,14 @@
+void main() {
+  for (int i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      print('FizzBuzz');
+    } else if (i % 3 == 0) {
+      print('Fizz');
+    } else if (i % 5 == 0) {
+      print('Buzz');
+    } else {
+      print(i);
+    }
+  }
+}
+//Думал написать что нибудь в комментариях , но вроде ничего сложного очень похоже на С#)
